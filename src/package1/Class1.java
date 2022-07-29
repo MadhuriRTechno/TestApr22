9795a445b45e517ddfdb9c1a33adf73915bde432
Madhuri Rajole <rajolemadhuri95@gmail.com>
2022-07-29 19:07:13 +0530

    updated C Code

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,10 +3,9 @@ package package1;
 public class Class1 {
 	public static void main(String[] args) 
 	{
-		System.out.println("branch A");
-		System.out.println("Hello");
-		System.out.println("Hiii");
-		System.out.println("project upload branch B");
-		System.out.println("project----- B");
+		System.out.println("branch C");
+		System.out.println("C1");
+		System.out.println("C2");
+		
 	}
 }
