e8e89cecbb97d430bd3defa810ddf8265e21d288
Madhuri Rajole <rajolemadhuri95@gmail.com>
2022-07-29 21:44:52 +0530

    A1 changes

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -3,6 +3,7 @@ package package1;
 public class Class2 {
 	public static void main(String[] args) 
 	{
+		System.out.println("HHIIIIIIIA11111");
 		System.out.println("Project on A1");
 		System.out.println("Hello");
 		System.out.println("Hiii");
