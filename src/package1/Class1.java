c3ba94f58c2a6d8bc5c537e1d1d48ac1b3b32703
Madhuri Rajole <rajolemadhuri95@gmail.com>
2022-07-29 22:12:57 +0530

    changes in A branch

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,6 +3,7 @@ package package1;
 public class Class1 {
 	public static void main(String[] args) 
 	{
+		System.out.println("upload project on A");
 		System.out.println("branch A");
 		System.out.println("Hello");
 		System.out.println("Hiii");
