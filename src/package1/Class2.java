a139e666dd7a96838677c11697e8091779cb0ce2
Madhuri Rajole <rajolemadhuri95@gmail.com>
2022-07-29 21:53:18 +0530

    on ch branch

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -4,5 +4,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hiii");
+		System.out.println("ONB11111");
+		System.out.println("upload on C");
 	}
 }
