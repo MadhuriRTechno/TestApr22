e7c9cdbf8b2eeddbe40c297f5088650ab86d4eae
Madhuri Rajole <rajolemadhuri95@gmail.com>
2022-07-29 19:21:26 +0530

    ChangesINC

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,11 +5,6 @@ public class Class1 {
 		System.out.println("branch C");
 		System.out.println("C1");
 		System.out.println("C2");
-		System.out.println("Project upload on branch A1");
-		System.out.println("branch A");
-		System.out.println("Hello");
-		System.out.println("Hiii");
-		System.out.println("project upload branch B");
-		System.out.println("project----- B");
+		
 	}
 }
